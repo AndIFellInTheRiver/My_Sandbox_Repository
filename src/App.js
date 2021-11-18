c42d30568aaceafff07b165c930897ba0f3a76d2
AndIFellInTheRiver <61436979+AndIFellInTheRiver@users.noreply.github.com>
2021-11-17 17:17:16 -0800

    Greeting

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>How is it going?</h1>
+      <h1>Greetings~<h1>
       <h2>Year:2023 High: OTHS</h2>
     </div>
   );
